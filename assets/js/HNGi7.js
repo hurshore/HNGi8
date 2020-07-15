4590c51bba44b8cda29584cad5c36105dcbc070b
faniran akinkunmi <tobifaniran@gmail.com>
2020-07-15 22:48:55 +0100

    Hngi7 and Faqs

---
+++ assets/js/HNGi7.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += increment;
+     obj.textContent = current;
+     if (current == end) {
+      clearInterval(timer);
+     }
+    }, step);
+  }
+  counter("counter-1", 500, 10000, 1000);
+  counter("counter-2", 0, 150, 3000);
+  counter("counter-3", 0, 350, 3000);
+  counter("counter-4", 0, 300, 3000);
+ });
+
+ $(document).ready(function() {
+  $('#autoWidth').lightSlider({
+      autoWidth:true,
+      loop:true,
+      onSliderLoad: function() {
+          $('#autoWidth').removeClass('cS-hidden');
+      } 
+  });  
+});
+
+ 
